f18a99485f4835cebedf64c3afe37721766285e4
artiom.43 <artiom.43@yandex.ru>
2022-09-26 20:28:01 +0300

    Add filter_list_by_list task

--- 01.2.BasicTypes/filter_list_by_list/filter_list_by_list.py
+++ 01.2.BasicTypes/filter_list_by_list/filter_list_by_list.py
@@ -5,3 +5,18 @@ def filter_list_by_list(lst_a: list[int] | range, lst_b: list[int] | range) -> l
     :param lst_b: second sorted list
     :return: filtered sorted list
     """
+    list_answer = []
+    index_a = 0
+    index_b = 0
+    while index_a < len(lst_a) and index_b < len(lst_b):
+        if lst_a[index_a] < lst_b[index_b]:
+            list_answer.append(lst_a[index_a])
+            index_a += 1
+        elif lst_a[index_a] > lst_b[index_b]:
+            index_b += 1
+        else:
+            index_a += 1
+    while index_a < len(lst_a):
+        list_answer.append(lst_a[index_a])
+        index_a += 1
+    return list_answer
