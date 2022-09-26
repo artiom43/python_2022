b06cd8ed3b305363c9a5899bb9837895358da538
artiom.43 <artiom.43@yandex.ru>
2022-09-26 13:23:54 +0300

    Add bin_basic task

--- 01.2.BasicTypes/bin_basic/bin_basic.py
+++ 01.2.BasicTypes/bin_basic/bin_basic.py
@@ -5,3 +5,14 @@ def find_value(nums: list[int] | range, value: int) -> bool:
     :param value: integer to find
     :return: True if value exists, False otherwise
     """
+    left_index = -1
+    right_index = len(nums)
+    while right_index - left_index > 1:
+        mid_index = (right_index + left_index)//2
+        if nums[mid_index] == value:
+            return True
+        if nums[mid_index] < value:
+            left_index = mid_index
+        else:
+            right_index = mid_index
+    return False
