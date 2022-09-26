fe75346d4fa3ccd1da9d6af39edcdb7017e83a73
artiom.43 <artiom.43@yandex.ru>
2022-09-26 15:56:34 +0300

    Add fizz_buzz task

--- 01.2.BasicTypes/fizz_buzz/fizz_buzz.py
+++ 01.2.BasicTypes/fizz_buzz/fizz_buzz.py
@@ -1,3 +1,7 @@
+from typing import List
+from typing import Union
+
+
 def get_fizz_buzz(n: int) -> list[int | str]:
     """
     If value divided by 3 - "Fizz",
@@ -7,3 +11,14 @@ def get_fizz_buzz(n: int) -> list[int | str]:
     :param n: size of sequence
     :return: list of values.
     """
+    fizz_buzz_list: List[Union[int, str]] = []
+    # fizz_buzz_list = []
+    for index in range(1, n + 1):
+        fizz_buzz_list.append(int(index))
+        if index % 3 == 0 and index % 5 != 0:
+            fizz_buzz_list[index - 1] = "Fizz"
+        if index % 3 == 0 and index % 5 == 0:
+            fizz_buzz_list[index - 1] = "FizzBuzz"
+        if index % 3 != 0 and index % 5 == 0:
+            fizz_buzz_list[index - 1] = "Buzz"
+    return fizz_buzz_list
