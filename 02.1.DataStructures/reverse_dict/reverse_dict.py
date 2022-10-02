92b854065405fcad46d630b25be5703086b265f6
artiom.43 <artiom.43@yandex.ru>
2022-10-02 23:00:26 +0300

    Add reverse_dict task

--- 02.1.DataStructures/reverse_dict/reverse_dict.py
+++ 02.1.DataStructures/reverse_dict/reverse_dict.py
@@ -6,3 +6,9 @@ def revert(dct: tp.Mapping[str, str]) -> dict[str, list[str]]:
     :param dct: dictionary to revert in format {key: value}
     :return: reverted dictionary {value: [key1, key2, key3]}
     """
+    reverted_dct: dict[str, list[str]] = {}
+    for key in dct:
+        reverted_dct[dct[key]] = []
+    for key in dct:
+        reverted_dct[dct[key]].append(key)
+    return reverted_dct
