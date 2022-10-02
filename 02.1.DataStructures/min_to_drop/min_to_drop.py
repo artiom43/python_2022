94f655e870eac9047a704ef0932dbf99edf4bd55
artiom.43 <artiom.43@yandex.ru>
2022-10-02 23:13:21 +0300

    Add min_to_drop task

--- 02.1.DataStructures/min_to_drop/min_to_drop.py
+++ 02.1.DataStructures/min_to_drop/min_to_drop.py
@@ -1,8 +1,15 @@
 import typing as tp
 
+from collections import Counter
+
 
 def get_min_to_drop(seq: tp.Sequence[tp.Any]) -> int:
     """
     :param seq: sequence of elements
     :return: number of elements need to drop to leave equal elements
     """
+    seq_counter = Counter(seq)
+    most_frequency = 0
+    for key in seq_counter:
+        most_frequency = max(most_frequency, seq_counter[key])
+    return len(seq) - most_frequency
