ffb99e1c6fc1e4ab690446763f44eebef760341f
artiom.43 <artiom.43@yandex.ru>
2022-10-03 17:32:45 +0300

    Add traverse_dictionary task

--- 02.1.DataStructures/traverse_dictionary/traverse_dictionary.py
+++ 02.1.DataStructures/traverse_dictionary/traverse_dictionary.py
@@ -1,6 +1,9 @@
 import typing as tp
 
 
+from collections import deque
+
+
 def traverse_dictionary_immutable(
         dct: tp.Mapping[str, tp.Any],
         prefix: str = "") -> list[tuple[str, int]]:
@@ -9,6 +12,13 @@ def traverse_dictionary_immutable(
     :param prefix: prefix for key used for passing total path through recursion
     :return: list with pairs: (full key from root to leaf joined by ".", value)
     """
+    result: list[tuple[str, int]] = []
+    for key in dct:
+        if type(dct[key]) == int:
+            result = result + [(prefix + key, dct[key])]
+        else:
+            result = result + traverse_dictionary_immutable(dct[key], prefix + key + ".")
+    return result
 
 
 def traverse_dictionary_mutable(
@@ -21,6 +31,12 @@ def traverse_dictionary_mutable(
     :param prefix: prefix for key used for passing total path through recursion
     :return: None
     """
+    for key in dct:
+        if type(dct[key]) == int:
+            result.append((prefix + key, dct[key]))
+        else:
+            traverse_dictionary_mutable(dct[key], result,  prefix + key + ".")
+    return None
 
 
 def traverse_dictionary_iterative(
@@ -30,3 +46,14 @@ def traverse_dictionary_iterative(
     :param dct: dictionary of undefined depth with integers or other dicts as leaves with same properties
     :return: list with pairs: (full key from root to leaf joined by ".", value)
     """
+    deq = deque([(dct, str(""))])
+    result = []
+    while deq:
+        dct_new, prefix = deq[0]
+        deq.popleft()
+        for key in dct_new:
+            if type(dct_new[key]) == int:
+                result.append((prefix + key, dct_new[key]))
+            else:
+                deq.append((dct_new[key], prefix + key + "."))
+    return result
