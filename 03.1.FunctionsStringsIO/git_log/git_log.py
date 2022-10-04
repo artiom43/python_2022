172810aa56c6ef113ddbe45aaa5569d97c711e17
artiom.43 <artiom.43@yandex.ru>
2022-10-04 23:52:39 +0300

    Add git_log task

--- 03.1.FunctionsStringsIO/git_log/git_log.py
+++ 03.1.FunctionsStringsIO/git_log/git_log.py
@@ -7,3 +7,11 @@ def reformat_git_log(inp: tp.IO[str], out: tp.IO[str]) -> None:
     Expected input format: `<sha-1>\t<date>\t<author>\t<email>\t<message>`
     Output format: `<first 7 symbols of sha-1>.....<message>`
     """
+    while True:
+        input_log = inp.readline()
+        if not input_log:
+            break
+        splitted_log = input_log.split("\t")
+        # print(" ".join(splitted_log[index] for index in range(10, len(splitted_log))))
+        out.write(splitted_log[0][0:7]+splitted_log[4].rjust(74, '.'))
+    return None
