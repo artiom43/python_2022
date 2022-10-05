964e96d6fe6d50da535111a76b8e83d7e4b76e8e
artiom.43 <artiom.43@yandex.ru>
2022-10-05 12:27:15 +0300

    Add caesar_cipher task

--- 03.1.FunctionsStringsIO/caesar_cipher/caesar_cipher.py
+++ 03.1.FunctionsStringsIO/caesar_cipher/caesar_cipher.py
@@ -1,3 +1,6 @@
+import string
+
+
 def caesar_encrypt(message: str, n: int) -> str:
     """Encrypt message using caesar cipher
 
@@ -5,3 +8,14 @@ def caesar_encrypt(message: str, n: int) -> str:
     :param n: shift
     :return: encrypted message
     """
+    alphabet_lw = string.ascii_lowercase
+    alphabet_up = string.ascii_uppercase
+    encrypted_message = ""
+    for letter in message:
+        if letter in alphabet_lw:
+            encrypted_message = encrypted_message + alphabet_lw[(alphabet_lw.index(letter) + n) % 26]
+        elif letter in alphabet_up:
+            encrypted_message = encrypted_message + alphabet_up[(alphabet_up.index(letter) + n) % 26]
+        else:
+            encrypted_message = encrypted_message + letter
+    return encrypted_message
