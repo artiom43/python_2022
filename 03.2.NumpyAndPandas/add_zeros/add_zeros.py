b3aab25c04af13776aa15710a8fe74793d9ee64b
artiom.43 <artiom.43@yandex.ru>
2022-10-10 22:50:27 +0300

    Add add_zeros task

--- 03.2.NumpyAndPandas/add_zeros/add_zeros.py
+++ 03.2.NumpyAndPandas/add_zeros/add_zeros.py
@@ -8,3 +8,11 @@ def add_zeros(x: npt.NDArray[np.int_]) -> npt.NDArray[np.int_]:
     :param x: array,
     :return: array with zeros inserted
     """
+    y: npt.NDArray[np.int_] = np.zeros((x.shape[0], 1), dtype=np.int_)
+    x = x.reshape((x.shape[0], 1))
+    # print(x)
+    # print(y)
+    answer = np.concatenate((x, y), axis=1)
+    # print(answer)
+    # print(answer.reshape(x.shape[0]*2))
+    return answer.reshape(x.shape[0]*2)[:-1]
