703824f5f9044c2e55f588abfb432849034ac1ce
artiom.43 <artiom.43@yandex.ru>
2022-10-10 21:01:23 +0300

    Add nearest_value task

--- 03.2.NumpyAndPandas/nearest_value/nearest_value.py
+++ 03.2.NumpyAndPandas/nearest_value/nearest_value.py
@@ -10,3 +10,12 @@ def nearest_value(matrix: npt.NDArray[np.float_], value: float) -> float | None:
     :param value: value to find
     :return: nearest value in matrix or None
     """
+    if matrix.shape[1] == 0:
+        return None
+    pos = np.amin(matrix, where=matrix >= value, initial=matrix.max())
+    otr = np.amax(matrix, where=matrix <= value, initial=matrix.min())
+    # print(pos, otr)
+    if pos - value > value - otr:
+        return float(otr)
+    else:
+        return float(pos)
