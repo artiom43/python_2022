dd270ace367aaa288b769f0e862fdddc5a366ba9
artiom.43 <artiom.43@yandex.ru>
2022-10-10 21:22:57 +0300

    Add max_element task

--- 03.2.NumpyAndPandas/max_element/max_element.py
+++ 03.2.NumpyAndPandas/max_element/max_element.py
@@ -9,3 +9,8 @@ def max_element(array: npt.NDArray[np.int_]) -> int | None:
     :param array: array,
     :return: max element value or None
     """
+    bool_arr = np.concatenate((np.array([False]), np.where(array == 0, True, False)), axis=0)[0:-1]
+    print(bool_arr)
+    if not bool_arr.max():
+        return None
+    return int(np.amax(array, where=bool_arr, initial=array.min()))
