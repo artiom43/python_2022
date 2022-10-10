00616a33f21a05a82aa36216511c2509a562f2e5
artiom.43 <artiom.43@yandex.ru>
2022-10-10 23:29:51 +0300

    Add vander task

--- 03.2.NumpyAndPandas/vander/vander.py
+++ 03.2.NumpyAndPandas/vander/vander.py
@@ -8,3 +8,11 @@ def vander(array: npt.NDArray[np.float_ | np.int_]) -> npt.NDArray[np.float_]:
     :param array: input array,
     :return: vandermonde matrix
     """
+    array = array.reshape((array.shape[0], 1))
+    print(array.shape[1])
+    ar_zer = np.zeros((array.shape[0]))
+    ar_ones = np.ones((array.shape[0]))*(array.shape[0] - 1)
+    print(ar_zer, ar_ones)
+    answer = np.logspace(ar_zer, ar_ones, num=array.shape[0], base=array, axis=1)
+    print(answer)
+    return answer
