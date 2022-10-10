7d835bc798e2edafa225e2247ecb8c9595e14d2a
artiom.43 <artiom.43@yandex.ru>
2022-10-10 20:19:48 +0300

    Add normalize_path task

--- 03.2.NumpyAndPandas/replace_nans/replace_nans.py
+++ 03.2.NumpyAndPandas/replace_nans/replace_nans.py
@@ -9,3 +9,15 @@ def replace_nans(matrix: npt.NDArray[np.float_]) -> npt.NDArray[np.float_]:
     :param matrix: matrix,
     :return: replaced matrix
     """
+    bool_m = np.isnan(matrix)
+    shape_arr = matrix.shape
+    print(np.count_nonzero(bool_m))
+    if np.count_nonzero(bool_m) == shape_arr[0]*shape_arr[1]:
+        return np.nan_to_num(matrix, nan=0)
+    # print(np.mean(matrix, where=bool_m))
+    # print(np.nan_to_num(matrix, nan=np.mean(matrix)))
+    # return np.nan_to_num(matrix, nan=np.nanmean(matrix, dtype=np.float_))
+    value = np.nanmean(matrix)
+    np.place(matrix, bool_m, value)
+    print(matrix)
+    return matrix
