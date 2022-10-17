1b8ca5e8760bac0858bb598f7a04d61c156236a4
artiom.43 <artiom.43@yandex.ru>
2022-10-17 17:43:15 +0300

    Add byteme task

--- 04.2.Bytecode/byteme/byteme.py
+++ 04.2.Bytecode/byteme/byteme.py
@@ -3,48 +3,86 @@ from typing import Any
 
 
 def f0() -> Any:
+    return None
     pass
 
 
 def f1() -> Any:
+    a = 0
+    return a
     pass
 
 
 def f2() -> Any:
+    a = 0
+    print(a)
+    return
     pass
 
 
 def f3() -> Any:
+    a = 0
+    a += 1
+    print(a)
+    return None
     pass
 
 
 def f4() -> Any:
+    return range(10)
     pass
 
 
 def f5() -> Any:
+    for i in range(10):
+        print(i)
+    return None
     pass
 
 
 def f6() -> Any:
+    a = 0
+    for i in range(10):
+        a += 1
+    print(a)
+    return None
     pass
 
 
 def f8() -> Any:
+    x, y = (1, 2)
+    return None
     pass
 
 
 def f9() -> Any:
+    if 1 == 1:
+        return 1
+    else:
+        return 2
     pass
 
 
 def f10() -> Any:
+    for i in range(10):
+        if i == 3:
+            return None
+    return None
     pass
 
 
 def f11() -> Any:
+    list_ = [1, 2, 3]
+    dict_ = {'a' : 1, 'b' : 2}
+    return (list_, dict_)
     pass
 
 
 def f12() -> Any:
+    a = 1
+    b = 2
+    c = 3
+    d = 4
+    e = 5
+    return a + (b*c)/(d**e)
     pass
