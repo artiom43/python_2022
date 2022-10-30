7bccb6aebd4c7fef72512de45da10ce8960457f6
artiom.43 <artiom.43@yandex.ru>
2022-10-30 22:13:21 +0300

    Add context_manager task

--- 06.2.ExceptionsContextManagers/context_manager/context_manager.py
+++ 06.2.ExceptionsContextManagers/context_manager/context_manager.py
@@ -1,17 +1,32 @@
+import traceback
 from contextlib import contextmanager
 from typing import Iterator, TextIO, Type
+import sys
 
 
 @contextmanager
 def supresser(*types_: Type[BaseException]) -> Iterator[None]:
-    pass
+    try:
+        yield None
+    except types_:
+        pass
 
 
 @contextmanager
 def retyper(type_from: Type[BaseException], type_to: Type[BaseException]) -> Iterator[None]:
-    pass
+    try:
+        yield None
+    except type_from as e:
+        raise type_to(*e.args)
 
 
 @contextmanager
 def dumper(stream: TextIO | None = None) -> Iterator[None]:
-    pass
+    try:
+        yield None
+    finally:
+        if stream is not None and sys.exc_info()[1] is not None:
+            stream.write(traceback.format_exception_only(sys.exc_info()[0], value=sys.exc_info()[1])[0])
+        elif sys.exc_info()[1] is not None:
+            sys.stderr.write(traceback.format_exception_only(sys.exc_info()[0], value=sys.exc_info()[1])[0])
+        pass
