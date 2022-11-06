3dfed1aaa1b4568b67c9655a0d7b396b0ca1665a
artiom.43 <artiom.43@yandex.ru>
2022-11-06 12:54:43 +0300

    Add flat_it task

--- 07.1.IteratorsGenerators/flat_it/flat_it.py
+++ 07.1.IteratorsGenerators/flat_it/flat_it.py
@@ -1,4 +1,5 @@
 from typing import Iterable, Generator, Any
+# from collections.abc import Iterable
 
 
 def flat_it(sequence: Iterable[Any]) -> Generator[Any, None, None]:
@@ -6,3 +7,28 @@ def flat_it(sequence: Iterable[Any]) -> Generator[Any, None, None]:
     :param sequence: sequence with arbitrary level of nested iterables
     :return: generator producing flatten sequence
     """
+    listt = []
+    answer = []
+    listt.append(sequence)
+    # print(sequence)
+    # return
+    while len(listt) != 0:
+        index = listt.pop()
+        # # print(index)
+        # if type(index) == str and len(index) == 1:
+        #     answer.append(index)
+        #     continue
+        if isinstance(index, Iterable) and not (type(index) == str and len(index) == 1):
+            for itera in index:
+                listt.append(itera)
+        else:
+            answer.append(index)
+        # try:
+        #     # yield index
+        #     some_obj = iter(index)
+        #     for itera in index:
+        #         listt.append(itera)
+        # except TypeError:
+        #     answer.append(index)
+    for index in reversed(answer):
+        yield index
