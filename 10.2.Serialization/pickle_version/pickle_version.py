1fe3885541f3bd39708d52142809f77713090c1c
artiom.43 <artiom.43@yandex.ru>
2022-11-28 13:17:32 +0300

    Add pickle_version task

--- 10.2.Serialization/pickle_version/pickle_version.py
+++ 10.2.Serialization/pickle_version/pickle_version.py
@@ -3,8 +3,8 @@ import dataclasses
 
 @dataclasses.dataclass
 class PickleVersion:
-	is_new_format: bool
-	version: int
+    is_new_format: bool
+    version: int
 
 
 def get_pickle_version(data: bytes) -> PickleVersion:
@@ -14,3 +14,14 @@ def get_pickle_version(data: bytes) -> PickleVersion:
     :param data: serialized object in pickle format.
     :return: protocol version.
     """
+    print(data[1: 2])
+    version = data[1: 2]
+    if version == b'\x02':
+        return PickleVersion(True, 2)
+    if version == b'\x03':
+        return PickleVersion(True, 3)
+    if version == b'\x04':
+        return PickleVersion(True, 4)
+    if version == b'\x05':
+        return PickleVersion(True, 5)
+    return PickleVersion(False, -1)
