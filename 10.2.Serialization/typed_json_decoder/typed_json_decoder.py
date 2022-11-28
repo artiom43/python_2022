5369042c2aa78c4e58f3e0136a68e113f780379b
artiom.43 <artiom.43@yandex.ru>
2022-11-28 12:31:50 +0300

    Add typed_json task

--- 10.2.Serialization/typed_json_decoder/typed_json_decoder.py
+++ 10.2.Serialization/typed_json_decoder/typed_json_decoder.py
@@ -3,6 +3,31 @@ import json
 
 from decimal import Decimal
 
+
+def as_special_type(dct: tp.Any) -> tp.Any:
+    val = dct.get("__custom_key_type__")
+    dct1: tp.Any = {}
+    # print(dct, " try ")
+    if val is not None:
+        if val == 'int':
+            for key in dct:
+                if key != "__custom_key_type__":
+                    dct1[int(key)] = dct[key]
+
+        if val == 'float':
+            for key in dct:
+                if key != "__custom_key_type__":
+                    dct1[float(key)] = dct[key]
+
+        if val == 'decimal':
+            for key in dct:
+                if key != "__custom_key_type__":
+                    dct1[Decimal(key)] = dct[key]
+        return dct1
+    else:
+        return dct
+
+
 def decode_typed_json(json_value: str) -> tp.Any:
     """
     Returns deserialized object from json string.
@@ -11,3 +36,8 @@ def decode_typed_json(json_value: str) -> tp.Any:
     :param json_value: serialized object in json format
     :return: deserialized object
     """
+
+    deserialized_obj = json.loads(json_value, object_hook=as_special_type)
+    # print(deserialized_obj)
+    # print(json_value)
+    return deserialized_obj
