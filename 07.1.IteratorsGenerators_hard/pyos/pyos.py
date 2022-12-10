3598300bed4fa464a8d5f85e1c62146023113840
artiom.43 <artiom.43@yandex.ru>
2022-12-10 18:08:08 +0300

    Add pyos task

--- 07.1.IteratorsGenerators_hard/pyos/pyos.py
+++ 07.1.IteratorsGenerators_hard/pyos/pyos.py
@@ -7,7 +7,7 @@ class SystemCall(ABC):
     """SystemCall yielded by Task to handle with Scheduler"""
 
     @abstractmethod
-    def handle(self, scheduler: 'Scheduler', task: 'Task') -> bool:
+    def handle(self, scheduler: 'Scheduler', task: 'Task') -> bool | int:
         """
         :param scheduler: link to scheduler to manipulate with active tasks
         :param task: task which requested the system call
@@ -25,24 +25,30 @@ class Task:
         :param target: coroutine to run. Coroutine can produce system calls.
         System calls are being executed by scheduler and the result sends back to coroutine.
         """
+        self.task_id = task_id
+        self.target = target
+        self.last_syscall_result = None
 
     def set_syscall_result(self, result: Any) -> None:
         """
         Saves result of the last system call
         """
+        self.last_syscall_result = result
 
     def step(self) -> SystemCall | None:
         """
         Performs one step of coroutine, i.e. sends result of last system call
         to coroutine (generator), gets yielded value and returns it.
         """
+        result = self.target.send(self.last_syscall_result)
+        return result
 
 
 class Scheduler:
     """Scheduler to manipulate with tasks"""
 
     def __init__(self) -> None:
-        self.task_id = 0
+        self.task_id = 1
         self.task_queue: Queue[Task] = Queue()
         self.task_map: dict[int, Task] = {}  # task_id -> task
         self.wait_map: dict[int, list[Task]] = {}  # task_id -> list of waiting tasks
@@ -51,12 +57,18 @@ class Scheduler:
         """Add task into task queue
         :param task: task to schedule for execution
         """
+        self.task_queue.put(task)
 
     def new(self, target: Coroutine) -> int:
         """Create and schedule new task
         :param target: coroutine to wrap in task
         :return: id of newly created task
         """
+        new_task = Task(self.task_id, target)
+        self._schedule_task(new_task)
+        self.task_map[self.task_id] = new_task
+        self.task_id += 1
+        return self.task_id - 1
 
     def exit_task(self, task_id: int) -> bool:
         """PRIVATE API: can be used only from scheduler itself or system calls
@@ -77,6 +89,40 @@ class Scheduler:
         handles them and reschedules task if needed
         :param ticks: number of iterations (task steps), infinite if not passed
         """
+        i = 0
+        while (ticks is not None and i < ticks) or ticks is None:
+            i += 1
+            if self.task_queue.empty():
+                continue
+            task_to_run = self.task_queue.get()
+            # print("task_id  ", task_to_run.task_id)
+            if task_to_run.task_id not in self.task_map:
+                continue
+            try:
+                x = task_to_run.step()
+                if x is not None:
+                    tr = x.handle(self, task_to_run)
+                    task_to_run.set_syscall_result(tr)
+                    # print(tr)
+                    if isinstance(x, WaitTask) and tr:
+                        continue
+
+            # print(self.task_queue, "Queue")
+            except StopIteration:
+                del self.task_map[task_to_run.task_id]
+                try:
+                    for task_to_reschedule in self.wait_map[task_to_run.task_id]:
+                        self.task_queue.put(task_to_reschedule)
+                except KeyError:
+                    pass
+                continue
+            # try:
+            #     for task_to_reschedule in self.wait_map[task_to_run.task_id]:
+            #         self.task_queue.put(task_to_reschedule)
+            # except KeyError:
+            #     pass
+            # print(x)
+            self.task_queue.put(task_to_run)
 
     def empty(self) -> bool:
         """Checks if there are some scheduled tasks"""
@@ -86,7 +132,9 @@ class Scheduler:
 class GetTid(SystemCall):
     """System call to get current task id"""
 
-    def handle(self, scheduler: Scheduler, task: Task) -> bool:
+    def handle(self, scheduler: Scheduler, task: Task) -> int | bool:
+        # return False
+        return task.task_id
         pass
 
 
@@ -96,7 +144,8 @@ class NewTask(SystemCall):
     def __init__(self, target: Coroutine) -> None:
         self.target = target
 
-    def handle(self, scheduler: Scheduler, task: Task) -> bool:
+    def handle(self, scheduler: Scheduler, task: Task) -> int | bool:
+        return scheduler.new(self.target)
         pass
 
 
@@ -106,8 +155,13 @@ class KillTask(SystemCall):
     def __init__(self, task_id: int) -> None:
         self.task_id = task_id
 
-    def handle(self, scheduler: Scheduler, task: Task) -> bool:
-        pass
+    def handle(self, scheduler: Scheduler, task: Task) -> bool | int:
+        try:
+            scheduler.task_map[self.task_id].target.close()
+            del scheduler.task_map[self.task_id]
+        except KeyError:
+            pass
+        return False
 
 
 class WaitTask(SystemCall):
@@ -116,7 +170,14 @@ class WaitTask(SystemCall):
     def __init__(self, task_id: int) -> None:
         self.task_id = task_id
 
-    def handle(self, scheduler: Scheduler, task: Task) -> bool:
+    def handle(self, scheduler: Scheduler, task: Task) -> bool | int:
         # Note: One shouldn't reschedule task which is waiting for another one.
         # But one must reschedule task if task id to wait for is invalid.
+        if self.task_id not in scheduler.task_map:
+            return False
+        if self.task_id in scheduler.wait_map:
+            scheduler.wait_map[self.task_id].append(task)
+        else:
+            scheduler.wait_map[self.task_id] = [task]
         pass
+        return True
