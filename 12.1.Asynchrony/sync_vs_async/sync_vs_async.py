57ad7a5db3d27e0e2622632d837ae09ad9280e1c
artiom.43 <artiom.43@yandex.ru>
2022-12-11 15:14:47 +0300

    Add sync_vs_async task

--- 12.1.Asynchrony/sync_vs_async/sync_vs_async.py
+++ 12.1.Asynchrony/sync_vs_async/sync_vs_async.py
@@ -1,3 +1,6 @@
+import asyncio
+import multiprocessing
+
 import aiohttp
 import requests
 
@@ -9,6 +12,9 @@ async def async_fetch(session: aiohttp.ClientSession, url: str) -> str:
     :param url: target http url
     :return: fetched text
     """
+    async with session.get(url) as resp:
+        text = await resp.text()
+        return text
 
 
 async def async_requests(urls: list[str]) -> list[str]:
@@ -17,6 +23,21 @@ async def async_requests(urls: list[str]) -> list[str]:
     :param urls: list of http urls ot fetch
     :return: list of fetched texts
     """
+    # print(urls)
+    async with aiohttp.ClientSession() as session:
+        # answer = await async_fetch(session, urls[0])
+        # print(answer)
+        # print(urls)
+        # lst = urls
+        list_of_tasks = []
+        lst = urls
+        for i, url in enumerate(urls):
+            list_of_tasks.append(asyncio.create_task(async_fetch(session, url)))
+        for i, url in enumerate(urls):
+            lst[i] = await list_of_tasks[i]
+
+        # lst = await asyncio.gather((async_fetch(session, url) for url in urls))
+        return lst
 
 
 def sync_fetch(session: requests.Session, url: str) -> str:
@@ -26,6 +47,9 @@ def sync_fetch(session: requests.Session, url: str) -> str:
     :param url: target http url
     :return: fetched text
     """
+    with session.get(url) as resp:
+        text = resp.text
+        return text
 
 
 def threaded_requests(urls: list[str]) -> list[str]:
@@ -34,3 +58,8 @@ def threaded_requests(urls: list[str]) -> list[str]:
     :param urls: list of http urls ot fetch
     :return: list of fetched texts
     """
+    with requests.Session() as session:
+        list_of_data = [session for url in urls]
+        with multiprocessing.Pool(20) as pool:
+            results = pool.starmap(sync_fetch, zip(list_of_data, urls))
+        return results
