84765882d1f101f39650b76269d0d11ad377f3b7
artiom.43 <artiom.43@yandex.ru>
2022-12-20 14:15:02 +0300

    Add alphabet task

--- 02.1.DataStructures[hard]/alphabet/alphabet.py
+++ 02.1.DataStructures[hard]/alphabet/alphabet.py
@@ -15,6 +15,25 @@ def extract_alphabet(
     :param graph: graph with partial order
     :return: alphabet
     """
+    list_of_vertex = []
+    used_vertex = {}
+
+    def dfs(vertex: str) -> None:
+        used_vertex[vertex] = True
+        for neighbour in graph[vertex]:
+            if neighbour not in used_vertex:
+                dfs(neighbour)
+        list_of_vertex.append(vertex)
+
+    first_vertex = ""
+    for vertex in graph:
+        first_vertex = vertex
+        break
+    dfs(first_vertex)
+    answer = []
+    for vertex in reversed(list_of_vertex):
+        answer.append(vertex)
+    return answer
 
 
 def build_graph(
@@ -25,6 +44,21 @@ def build_graph(
     :param words: ordered words
     :return: graph
     """
+    graph: dict[str, set[str]] = {}
+    for word in words:
+        for letter in word:
+            graph[letter] = set()
+
+    for index in range(len(words) - 1):
+        first_word = words[index]
+        second_word = words[index + 1]
+        index_of_str = 0
+        while index_of_str < len(first_word) and index_of_str < len(second_word):
+            if first_word[index_of_str] != second_word[index_of_str]:
+                graph[first_word[index_of_str]].add(second_word[index_of_str])
+            else:
+                break
+    return graph
 
 
 #########################
