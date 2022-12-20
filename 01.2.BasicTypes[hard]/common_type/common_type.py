ffbb83f18a8766e1d6f9a96b4dcc3ff59527823d
artiom.43 <artiom.43@yandex.ru>
2022-12-20 13:00:05 +0300

    Add common_type task

--- 01.2.BasicTypes[hard]/common_type/common_type.py
+++ 01.2.BasicTypes[hard]/common_type/common_type.py
@@ -6,7 +6,7 @@ def get_common_type(type1: type, type2: type) -> type:
     :param type2: one of [bool, int, float, complex, list, range, tuple, str] types
     :return: the most concrete common type, which can be used to convert both input values
     """
-    list_of_types = [int, float, complex, str, list, tuple]
+    list_of_types = [int, float, complex, list, tuple, str]
     list_elements_of_types = {bool: True, int: 1, float: 0.1, complex: 2j, list: [3, 2, 3], tuple: (1, ), str: "wer"}
     if type1 == range:
         type1 = tuple
@@ -22,6 +22,9 @@ def get_common_type(type1: type, type2: type) -> type:
             continue
         except ValueError:
             continue
+        if type1 == float or type2 == float:
+            if common_type == int:
+                continue
         return common_type
     dict_of_types = {(type([]), type((1,))): list, (type((1,)), type([])): list}
     if (type1, type2) in dict_of_types:
@@ -29,4 +32,4 @@ def get_common_type(type1: type, type2: type) -> type:
     return str
 
 
-# print(get_common_type(str, int))
+print(get_common_type(float, int))
