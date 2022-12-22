16b6d17c58c6fc9f8d8b49db075d5319306cf15f
artiom.43 <artiom.43@yandex.ru>
2022-12-22 21:56:09 +0300

    Add /property_converter task

--- 14.2.MetaclassesAndDescriptors/property_converter/property_converter.py
+++ 14.2.MetaclassesAndDescriptors/property_converter/property_converter.py
@@ -1,4 +1,5 @@
 import typing as tp
+# from collections import defaultdict
 
 
 class MetaClass(type):
@@ -29,22 +30,30 @@ class MetaClass(type):
             old_class = base
             break
         # new_items = {}
-        getter = None
-        setter = None
-        name_get_or_set = ""
+        getter = {}
+        setter = {}
+        name_get_or_set = set()
         for name, value in old_class.__dict__.items():
             # print(name, value)
             if name[4:] in members:
                 continue
             if name.startswith("get"):
-                getter = value
-                name_get_or_set = name[4:]
+                getter[name[4:]] = value
+                name_get_or_set.add(name[4:])
             if name.startswith("set"):
-                setter = value
-                name_get_or_set = name[4:]
+                setter[name[4:]] = value
+                name_get_or_set.add(name[4:])
         # members[]
-        if name_get_or_set != "":
-            members[name_get_or_set] = property(getter, setter)
+        for name_get in name_get_or_set:
+            print(name_get)
+            f_get = None
+            f_set = None
+            if name_get in getter:
+                f_get = getter[name_get]
+            if name_get in setter:
+                f_set = setter[name_get]
+
+            members[name_get] = property(f_get, f_set)
         return type.__new__(cls, name, bases, members)
 
     # def __init__(cls, *args, **kwargs):
@@ -53,18 +62,18 @@ class MetaClass(type):
 
 
 class PropertyConverter(metaclass=MetaClass):
-    def __getattr__(self, item: str) -> tp.Any:
-        # print("sdf")
-        # if item in self.__dict__:
-        #     return self.__dict__[item]
-        # else:
-        #     raise AttributeError
-        return object.__getattribute__(self, item)
-
-    def __setattr__(self, key: str, value: tp.Any) -> None:
-        # pass
-        # setattr(self, key, value)
-        object.__setattr__(self, key, value)
+    # def __getattr__(self, item: str) -> tp.Any:
+    #     # print("sdf")
+    #     # if item in self.__dict__:
+    #     #     return self.__dict__[item]
+    #     # else:
+    #     #     raise AttributeError
+    #     return object.__getattribute__(self, item)
+    # 
+    # def __setattr__(self, key: str, value: tp.Any) -> None:
+    #     # pass
+    #     # setattr(self, key, value)
+    #     object.__setattr__(self, key, value)
     # def __int__(self):
     #     self.__dict__['__setattr__'] = object.__setattr__
     #     self.__dict__['__getattr__'] = object.__getattribute__
